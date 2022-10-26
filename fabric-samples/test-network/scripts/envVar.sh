fb7e5c7340d0284cf340fdef3b88af5c56993ad3
le99 <le.florez602@uniandes.edu.co>
2022-10-26 08:05:29 -0500

    removed tls

--- fabric-samples/test-network/scripts/envVar.sh
+++ fabric-samples/test-network/scripts/envVar.sh
@@ -10,7 +10,7 @@
 # imports
 . scripts/utils.sh
 
-export CORE_PEER_TLS_ENABLED=true
+export CORE_PEER_TLS_ENABLED=false
 export ORDERER_CA=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
 export PEER0_ORG1_CA=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
 export PEER0_ORG2_CA=${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
